474f2e4f79c25941b6310b5163364203e53ee78e
Sergey M <sergio007myronenko@gmail.com>
2022-01-21 18:56:04 +0200

    Resolve 2 Task

--- Index.js
+++ Index.js
@@ -2,8 +2,15 @@
 
 // Task 1 //
 
-const array1 = [1, 2, 3];
+/* const array1 = [1, 2, 3];
 const array2 = [4, 5, 6];
 
 const array3 = array1.concat(array2);
-console.log(array3); 
+console.log(array3); */
+
+// Task 2 //
+
+const array1 = [1, 2, 3];
+
+array1.reverse();
+console.log(array1);
